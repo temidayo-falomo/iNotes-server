b50558db8c735a4ef5958caec8001d9a3ac00601
temidayo-falomo <temmideee@gmail.com>
2022-11-14 00:47:06 +0100

    new updates

--- dist/Index.js
+++ dist/Index.js
@@ -7,14 +7,16 @@ var express_1 = __importDefault(require("express"));
 var mongoose_1 = __importDefault(require("mongoose"));
 var user_routes_1 = __importDefault(require("./routes/user-routes"));
 var cors_1 = __importDefault(require("cors"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 var app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://inotes-mini.netlify.app"],
 }));
 app.use(express_1.default.json());
 app.use("/api", user_routes_1.default);
 mongoose_1.default
-    .connect("mongodb+srv://temidayo:7zZKyWQYr2aOqOv1@inotes-cluster.rhbhyen.mongodb.net/?retryWrites=true&w=majority")
+    .connect("".concat(process.env.MONGODB_URL))
     .then(function () {
     app.listen(process.env.PORT || 5000);
 })
--- src/Index.ts
+++ src/Index.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user-routes";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app: Application = express();
 app.use(
@@ -13,9 +15,7 @@ app.use(express.json());
 app.use("/api", userRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://temidayo:7zZKyWQYr2aOqOv1@inotes-cluster.rhbhyen.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(`${process.env.MONGODB_URL}`)
   .then(() => {
     app.listen(process.env.PORT || 5000);
   })
